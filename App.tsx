d5a0daee03855d173fa4d967d077f7cccfd57b91
luwanise <praiseoladejo2003@gmail.com>
2025-01-31 05:03:07 -0600

    Update Button Urls

--- App.tsx
+++ App.tsx
@@ -5,9 +5,9 @@ const App = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>GitHub Repository</Text>
-      <Button title="Go to Repo" onPress={() => Linking.openURL('YOUR_GITHUB_REPO_LINK')} />
+      <Button title="Go to Repo" onPress={() => Linking.openURL('https://github.com/luwanise/myapp')} />
       <Text>HNG Hire Pages</Text>
-      <Button title="HNG Hire Pages" onPress={() => Linking.openURL('HNG_HIRE_LINK')} />
+      <Button title="HNG Hire Pages" onPress={() => Linking.openURL('https://hng.tech/hire/react-native-developers')} />
     </View>
   );
 };
